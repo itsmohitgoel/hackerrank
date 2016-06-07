e2eefc5a2bdb8c6259103394bbd7a924e72aea75
itsmohitgoel <itsmohitgoel@gmail.com>
2016-06-07 09:37:43 +0530

    Character class with negatition

---
+++ strings/character_class_with_negation/RegExDemo1.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegExDemo1 {
+	public String text;
+	public static void main(String[] args) {
+		RegExDemo1 textObject = new RegExDemo1();
+		List<String> tokens = new ArrayList<>();
+		textObject.text = new String("Splitting a string, it's as easy as 1 2 33!  Right?");
+		//Character classes with Negation
+		tokens = textObject.getTokens("[^a-z123 ]");
+		if(tokens != null){
+			System.out.println(tokens.toString());;
+		}
+	}
+	
+	public List<String> getTokens(String pattern){
+		Pattern regularExp = Pattern.compile(pattern);
+		Matcher matcher = regularExp.matcher(text);
+		List<String> tokensList = new ArrayList<>();
+		while (matcher.find()) {
+			tokensList.add(matcher.group());
+		}
+		return tokensList;
+	}
+
+}
